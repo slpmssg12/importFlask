e220bb625e70b149fa60536440547ac7f14b2f31
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-09-16 19:18:02 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(_name_) # "_main_"
+
+
+@app.route('/hello', methods=['GET'])
+def hello_world():
+ return "Hello world."
+
+if _name_ == "_main_":
+app.run(host='0.0.0.0', port=8080
