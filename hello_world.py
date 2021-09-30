91db60357c86e8cd9bbb506cc595cc43ec01173b
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-09-30 16:21:53 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,11 @@
 from flask import Flask
-app = Flask(_name_) # "_main_"
+app = Flask(__name__) # "__main__"
 
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
   return "Hello world."
 
-if _name_ == "_main_":
+if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080
   
