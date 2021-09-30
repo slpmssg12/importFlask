5057f73ea37e108de2a5071ea150918f1955f3e8
sherri lee <75329306+slpmssg12@users.noreply.github.com>
2021-09-30 16:08:07 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,8 @@ app = Flask(_name_) # "_main_"
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
- return "Hello world."
+  return "Hello world."
 
 if _name_ == "_main_":
- app.run(host='0.0.0.0', port=8080
- 
+  app.run(host='0.0.0.0', port=8080
+  
