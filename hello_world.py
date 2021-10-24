a0207432e7ac86aa8340f38ca843ebb9247ebcac
root <root@ip-172-31-11-154.us-east-2.compute.internal>
2021-10-24 17:40:07 +0000

    added comment

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,14 @@ app = Flask(__name__) # "__main__"
 
 @app.route('/hello', methods=['GET'])
 def hello_world():
-  return "Hello world."
+  return """<html>
+  <body>
+  <h1>hello world<h1>
+  </body>
+  </html>
+  """
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
   
+#comment added in aws 10/24 @1:38
